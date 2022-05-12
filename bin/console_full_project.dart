0809e69f1e848db29b04bb1b1beeed7b7db8f0ce
Diego Aarón <diegoaarondv@gmail.com>
2022-05-11 23:10:50 -0500

    update 2

--- bin/console_full_project.dart
+++ bin/console_full_project.dart
@@ -1,5 +1,11 @@
-import 'package:console_full_project/console_full_project.dart' as console_full_project;
+import 'package:console_full_project/console_full_project.dart'
+    as console_full_project;
 
 void main(List<String> arguments) {
-  print('Hello world: ${console_full_project.calculate()}!');
+  try {
+    print('Hello world: ${console_full_project.calculate()}! \n'
+        'Args sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}');
+  } catch (e) {
+    print('Error: At least one argument is not a intenger!');
+  }
 }

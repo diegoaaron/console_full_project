79f6e1e6c0ddb3c16f10a8f55a3a0a910e9bb2a4
Diego Aarón <diegoaarondv@gmail.com>
2022-05-14 23:48:18 -0500

    update packages

--- bin/console_full_project.dart
+++ bin/console_full_project.dart
@@ -3,7 +3,7 @@ import 'package:console_full_project/console_full_project.dart'
 
 void main(List<String> arguments) {
   try {
-    print('Hello world: ${console_full_project.calculate()}! \n'
+    print('Hello world: ${console_full_project.calculateAdd()}! \n'
         'Args sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}');
   } catch (e) {
     print('Error: At least one argument is not a intenger!');
--- lib/console_full_project.dart
+++ lib/console_full_project.dart
@@ -1,3 +1,17 @@
-int calculate() {
-  return 6 * 7;
+import 'package:calculator/calculator.dart';
+
+int calculateAdd() {
+  return add(25, 4);
+}
+
+int calculateSubtract() {
+  return subtract(25, 4);
+}
+
+int calculateMultiply() {
+  return multiply(25, 4);
+}
+
+double calculateDivide() {
+  return divide(25, 4);
 }

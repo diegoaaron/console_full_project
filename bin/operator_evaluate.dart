8f52a52fdd1665e97dbedfae1307c978146212ec
Diego Aarón <diegoaarondv@gmail.com>
2022-05-24 09:57:47 -0500

    update operator 2

--- bin/operator_evaluate.dart
+++ bin/operator_evaluate.dart
@@ -18,6 +18,20 @@ void operatorTest() {
 
   print('7/2 = $i r $r');
   print('$i * 2 + $r = 7');
+
+  int w = 10;
+
+  //! w1 = w, then w = w + 1
+
+  var w1 = w++;
+  print(w1);
+
+  //! w = w + 1, then w2 = w;
+
+  var w2 = ++w;
+  print(w2);
+
+  logicalOperator();
 }
 
 class X {
@@ -35,7 +49,25 @@ class B extends X {
   String operator +(X other) => 'B() + = ${value + other.value}';
 }
 
-
 //expresiones
-// las expresiones se crean al combinar el uso de operadores. 
+// las expresiones se crean al combinar el uso de operadores.
 
+// TYPE TEST OPERATORS
+// as  - Typecast (also used with library prefixes)
+// is  - True if the object has the specified type
+// is! - True if the object doesn't have the specified type
+
+// LOGICAL OPERATORS
+// !expr expression inverter
+// && logical AND
+// || logical OR
+
+void logicalOperator() {
+  var a = 5;
+  var isFalse;
+  if (a.isEven || a.isFinite && a.isNegative) {
+    isFalse = !(true);
+  }
+
+  print(isFalse);
+}

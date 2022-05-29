7a5ef0cfefaa33c81f191dda30a709c36656eb33
Diego Aarón <diegoaarondv@gmail.com>
2022-05-29 02:06:35 -0500

    update constructor

--- bin/classTwotest.dart
+++ bin/classTwotest.dart
@@ -1,4 +1,14 @@
 class A {
+  // constructor por defecto
+  A(int p, int a, int b, int c, int d, int e, int f)
+      : _private = p,
+        this.a = a,
+        this.b = b,
+        //this.c = c;
+        this.d = d,
+        this.e = e,
+        this.f = f;
+
   int? _private;
 
   int? a;
@@ -8,7 +18,7 @@ class A {
 
   late int d;
   late final int e;
-  late final int f = 5;
+  late final int f;
 
   static int g = 6;
   static late int h;
@@ -16,19 +26,19 @@ class A {
   static late final int j;
 
   static const int k = 10;
+
+  @override
+  String toString() =>
+      'A(_private: $_private, a: $a, b:$b, c: $c, d: $d, e: $e, f:$f';
 }
 
 void parania() {
-  var alfa = A();
-
+  var alfa = A(1, 2, 3, 4, 5, 6, 7);
+  print('alfa: $alfa');
   print('alfa._private; --> ${alfa._private}');
   print('alfa.a; --> ${alfa.a}');
   print('alfa.b; --> ${alfa.b}');
   print('alfa.c; --> ${alfa.c}');
-
-  alfa.d = 3;
   print('alfa.d; --> ${alfa.d}');
-
-  alfa.e = 4;
   print('alfa.e; --> ${alfa.e}');
 }
